4d482b853550d867cd7abb9aaae946f2fa374061
akhilkvnb <102411063+akhilkvnb@users.noreply.github.com>
2022-04-05 09:15:24 +0530

    Add files via upload

---
+++ resposive Website/script1.js
@@ -0,0 +1,44 @@
+function change(){
+   var images= document.getElementById('img1')
+   images.src="11.webp"
+
+   images= document.getElementById('img2')
+   images.src="22.webp"
+
+   images= document.getElementById('img3')
+   images.src="33.webp"
+
+   images= document.getElementById('img4')
+   images.src="44.webp"
+
+   images= document.getElementById('img5')
+   images.src="55.webp"
+
+   images= document.getElementById('img6')
+   images.src="66.webp"
+
+   images= document.getElementById('img7')
+   images.src="77.webp"
+
+}
+function change1(){
+    var images= document.getElementById('img1')
+    images.src="imgs/1.webp"
+ 
+    images= document.getElementById('img2')
+    images.src="imgs/2.webp"
+ 
+    images= document.getElementById('img3')
+    images.src="imgs/3.webp"
+ 
+    images= document.getElementById('img4')
+    images.src="imgs/4.webp"
+ 
+    images= document.getElementById('img5')
+    images.src="imgs/5.webp"
+ 
+    images= document.getElementById('img6')
+    images.src="imgs/6.webp"
+ 
+    
+ }
